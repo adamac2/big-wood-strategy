20676b877a9baef94c9bd556168e96ddc0cc187c
tdbeirne <tdbeirne@yahoo.com>
2019-03-03 16:45:03 -0600

    Commented out GUI in main game engine, and passed board state to python script.

--- wood-the-gathering-master/src/wood/game/WoodTheGathering.java
+++ wood-the-gathering-master/src/wood/game/WoodTheGathering.java
@@ -9,7 +9,7 @@ import javax.swing.*;
 
 public class WoodTheGathering {
     private static final int DEFAULT_BOARD_SIZE = 10;
-    private static final boolean DEFAULT_GUI_ENABLED = true;
+    private static final boolean DEFAULT_GUI_ENABLED = false;
     private static final int PREFERRED_GUI_WIDTH = 750; // Bump this up or down according to your screen size
 
     public static void main(String[] args) {
@@ -18,24 +18,34 @@ public class WoodTheGathering {
         final GameEngine gameEngine;
 
         if(replayFilePath == null) {
-            WoodPlayerStrategy redStrategy =  new RandomStrategy();
+            WoodPlayerStrategy redStrategy =  new TrainingStrategy();
             WoodPlayerStrategy blueStrategy = new RandomStrategy();
             gameEngine = new GameEngine(DEFAULT_BOARD_SIZE, redStrategy, blueStrategy);
             gameEngine.setGuiEnabled(guiEnabled);
         } else {
             gameEngine = ReplayIO.setupEngineForReplay(replayFilePath);
-            guiEnabled = true;
+            guiEnabled = false;
         }
 
         if(gameEngine == null) {
             return;
         }
 
+        //TODO reinstate if needed
+        /*
         if(guiEnabled) {
             // Run the GUI code on a separate Thread (The event dispatch thread)
             SwingUtilities.invokeLater(() -> UserInterface.instantiateGUI(gameEngine, PREFERRED_GUI_WIDTH));
         }
+        */
+
+        long startTime = System.nanoTime();
+
         gameEngine.runGame();
+
+        long endTime   = System.nanoTime();
+        long totalTime = endTime - startTime;
+        System.out.println(totalTime / 1000000000.0);
 
         // Record the replay if the output path isn't null and we aren't already watching a replay
         String replayOutputFilePath = null;
